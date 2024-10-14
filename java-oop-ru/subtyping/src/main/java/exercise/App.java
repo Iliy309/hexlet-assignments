6787697620247b3358edf57593b0258e0b11b288
@hexlet/cli <support@hexlet.io>
2024-10-14 23:42:20 +0500

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,12 +1,11 @@
 package exercise;
-
 // BEGIN
 public class App {
 
     public static void swapKeyValue(KeyValueStorage storage) {
         for (var items : storage.toMap().entrySet()) {
             storage.unset(items.getKey());
-            storage.set(items.getValue(),items.getKey());
+            storage.set(items.getValue(), items.getKey());
         }
     }
 }
