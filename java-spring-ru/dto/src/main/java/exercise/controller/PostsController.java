94a4a36219083074dc31a115a660411242bb8404
@hexlet/cli <support@hexlet.io>
2024-11-05 18:33:57 +0500

    submit java-spring-ru/dto

---
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,65 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+@RestController
+@RequestMapping(path = "/posts")
+public class PostsController {
+
+    @Autowired
+    private  PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping
+    public List<PostDTO> getAll(){
+        var posts  = postRepository.findAll();
+        return posts.stream()
+                .map(this::toPostDTO)
+                .toList();
+    }
+
+    @GetMapping(path = "/{id}")
+    public PostDTO getPost(@PathVariable Long id ){
+        var post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found"));
+        return toPostDTO(post);
+
+    }
+
+    public PostDTO toPostDTO(Post post){
+        var postDTO = new PostDTO();
+        postDTO.setId(post.getId());
+        postDTO.setTitle(post.getTitle());
+        postDTO.setBody(post.getBody());
+
+        var comments = commentRepository.findByPostId(post.getId());
+        var commentsDTO = comments.stream()
+                .map(comment -> {
+                    var commentDTO = new CommentDTO();
+                    commentDTO.setId(comment.getId());
+                    commentDTO.setBody(comment.getBody());
+                    return  commentDTO;
+                }).toList();
+        postDTO.setComments(commentsDTO);
+        return postDTO;
+    }
+
+
+}
+// END
---
+++ java-spring-ru/dto/src/main/java/exercise/model/Comment.java
@@ -0,0 +1,39 @@
+package exercise.model;
+
+import exercise.dto.CommentDTO;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.GeneratedValue;
+
+
+@Entity
+@Table(name = "comments")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    private long postId;
+    private String body;
+
+
+    public CommentDTO toDTO(){
+        var commentDTO = new CommentDTO();
+        commentDTO.setId(id);
+        commentDTO.setBody(body);
+
+        return commentDTO;
+    }
+}
