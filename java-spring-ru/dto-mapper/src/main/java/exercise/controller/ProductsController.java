745cfdc7b3598592f613837024dbeb8e5d4f256f
@hexlet/cli <support@hexlet.io>
2024-11-06 01:59:42 +0500

    submit java-spring-ru/dto-mapper

--- java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
@@ -1,13 +1,8 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,6 +42,7 @@ public class ProductsController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ProductDTO create(@RequestBody ProductCreateDTO productCreateDTO){
         var product = productMapper.map(productCreateDTO);
         productRepository.save(product);
--- java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -12,18 +12,19 @@ import org.mapstruct.*;
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
-public abstract class ProductMapper  {
-    @Mapping(target = "title", source = "name")
-    @Mapping(target = "price", source = "cost")
-    @Mapping(target = "vendorCode", source = "barcode")
-    public abstract Product map(ProductCreateDTO postCreateDto);
+public abstract class ProductMapper {
+
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "price", target = "cost")
+    @Mapping(source = "vendorCode", target = "barcode")
+    public abstract Product map(ProductCreateDTO dto);
+
+    @Mapping(source = "price", target = "cost")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
 
     @Mapping(target = "title", source = "name")
     @Mapping(target = "price", source = "cost")
     @Mapping(target = "vendorCode", source = "barcode")
-    public abstract ProductDTO map(Product product);
-
-    @Mapping(target = "price", source = "cost")
-    public abstract void update(ProductUpdateDTO productUpdateDTO, @MappingTarget Product model);
+    public abstract ProductDTO map(Product model);
 }
 // END
